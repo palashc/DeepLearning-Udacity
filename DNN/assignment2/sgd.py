28c392e4b9936e819b41614095ef5dc1c9603245
Palash Chauhan <palashc983@gmail.com>
2017-05-10 13:44:35 +0530

    few changes to perform experiments with various batch size

--- DNN/assignment2/sgd.py
+++ DNN/assignment2/sgd.py
@@ -17,14 +17,16 @@ with open(pickle_file, 'rb') as f:
   test_dataset = save['test_dataset']
   test_labels = save['test_labels']
   del save  # hint to help gc free up memory
-  print('Training set', train_dataset.shape, train_labels.shape)
-  print('Validation set', valid_dataset.shape, valid_labels.shape)
-  print('Test set', test_dataset.shape, test_labels.shape)
+  # print('Training set', train_dataset.shape, train_labels.shape)
+  # print('Validation set', valid_dataset.shape, valid_labels.shape)
+  # print('Test set', test_dataset.shape, test_labels.shape)
 
 
 
 image_size = 28
 num_labels = 10
+def accuracy(predictions, trueLabels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(trueLabels, 1)) / predictions.shape[0])
 
 def reformat(dataset, labels):
   dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
@@ -34,48 +36,59 @@ def reformat(dataset, labels):
 train_dataset, train_labels = reformat(train_dataset, train_labels)
 valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
 test_dataset, test_labels = reformat(test_dataset, test_labels)
-print('Training set', train_dataset.shape, train_labels.shape)
-print('Validation set', valid_dataset.shape, valid_labels.shape)
-print('Test set', test_dataset.shape, test_labels.shape)
+# print('Training set', train_dataset.shape, train_labels.shape)
+# print('Validation set', valid_dataset.shape, valid_labels.shape)
+# print('Test set', test_dataset.shape, test_labels.shape)
 
-batch_size = 128
-num_steps = 1001
+batch_size_array = [64, 128, 256, 512, 1024, 2048]
+num_steps_array = [5000]
+output_file = "out1.txt"
+f = open(output_file, "w")
 
-graph = tf.Graph()
-with graph.as_default():
-  tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size * image_size))
-  tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
-  tf_valid_dataset = tf.constant(valid_dataset)
-  tf_test_dataset = tf.constant(test_dataset)
+def accuracy(predictions, trueLabels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(trueLabels, 1)) / predictions.shape[0])
 
-  #parameters
-  weights = tf.Variable(tf.truncated_normal([image_size * image_size, num_labels])) 
-  biases = tf.Variable(tf.zeros([num_labels]))
+for batch_size in batch_size_array:
+  for num_steps in num_steps_array:
+    graph = tf.Graph()
+    with graph.as_default():
+      #Input
+      tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size * image_size))
+      tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+      tf_valid_dataset = tf.constant(valid_dataset)
+      tf_test_dataset = tf.constant(test_dataset)
 
-  #training
-  logits = tf.matmul(tf_train_dataset, weights) + biases
-  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+      #parameters
+      weights = tf.Variable(tf.truncated_normal([image_size * image_size, num_labels])) 
+      biases = tf.Variable(tf.zeros([num_labels]))
 
-  #optimizer
-  optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+      #training
+      logits = tf.matmul(tf_train_dataset, weights) + biases
+      loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
 
-  #predictions
-  train_prediction = tf.nn.softmax(logits)
-  valid_prediction = tf.nn.softmax(tf.matmul(tf_valid_dataset, weights) + biases)
-  test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
+      #optimizer
+      optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
+      #predictions
+      train_prediction = tf.nn.softmax(logits)
+      valid_prediction = tf.nn.softmax(tf.matmul(tf_valid_dataset, weights) + biases)
+      test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
 
-with tf.Session(graph=graph) as session:
-  tf.global_variables_initializer().run()
-  print("Initialized")
-  for step in range(num_steps):
-    offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
-    batch_data = train_dataset[offset:(offset + batch_size), :]
-    batch_labels = train_labels[offset:(offset + batch_size), :]
-    feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
-    _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
-    if (step % 500 == 0):
-      print("Minibatch loss at step %d: %f" % (step, l))
-      print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
-      print("Validation accuracy: %.1f%%" % accuracy(valid_prediction.eval(), valid_labels))
-  print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
+    start = time.time()
+
+    with tf.Session(graph=graph) as session:
+      tf.global_variables_initializer().run()
+      #print('Initialized!!!')
+      for step in range(num_steps):
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        batch_data = train_dataset[offset:(offset + batch_size), :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+        _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+        if step%500==0:
+          end = time.time()
+          print('BatchSize: %d NumSteps: %d TimeElapsed: %.2f Test accuracy: %.1f%%' % (batch_size, step, (end-start), accuracy(test_prediction.eval(), test_labels)))
+          f.write('BatchSize: %d NumSteps: %d TimeElapsed: %.2f Test accuracy: %.1f%%\n' % (batch_size, step, (end-start), accuracy(test_prediction.eval(), test_labels)))
+      
+      
+f.close()
